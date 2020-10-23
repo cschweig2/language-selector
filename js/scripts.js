399c2fe1dadb1f1b577f8899bc4e96d165c14cba
Chelsea Becker <cschweig2@gmail.com>
2020-10-23 12:43:58 -0700

    Add question 5, modify alert when radio buttons arent selected, and toggle the visibility of results to user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,89 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
+
+    if ($("input:radio[name=q1]:checked") != true) {
+      $("#warning").show();
+    } else if ($("input:radio[name=q2]:checked") != true) {
+      $("#warning").show();
+    } else if ($("input:radio[name=q3]:checked") != true) {
+      $("#warning").show();
+    } else if ($("input:radio[name=q4]:checked") != true) {
+      $("#warning").show();
+    } else if ($("input:radio[name=q5]:chedked") != true) {
+      $("#warning").show();
+    } else {
+      $("#warning").hide();
+    }
+
     const answer1 = $("input:radio[name=q1]:checked").val();
     const answer2 = $("input:radio[name=q2]:checked").val();
-    const answer3 = $("input:radio[name=q3]:checekd").val();
+    const answer3 = $("input:radio[name=q3]:checked").val();
     const answer4 = $("input:radio[name=q4]:checked").val();
+    const answer5 = $("input:radio[name=q5]:checked").val();
     
     let cSharp = 0;
     let javaScript = 0;
     let go = 0;
 
-    if(answer1 && answer2 && answer3 && answer4) {
-      $("#warning").hide();
-      
+    if (answer1 === "cSharp") {
+      cSharp++;
+    }
+    if (answer1 === "javaScript") {
+      javaScript++;
+    }
+    if (answer1 === "go") {
+      go++;
+    }
+    if (answer2 === "cSharp") {
+      cSharp++;
+    }
+    if (answer2 === "javaScript") {
+      javaScript++;
+    }
+    if (answer2 === "go") {
+      go++;
+    }
+    if (answer3 === "cSharp") {
+      cSharp++;
+    }
+    if (answer3 === "javaScript") {
+      javaScript++;
+    }
+    if (answer3 === "go") {
+      go++;
+    }
+    if (answer4 === "cSharp") {
+      cSharp++;
+    }
+    if (answer4 === "JavaScript") {
+      javaScript++;
+    }
+    if (answer4 === "go") {
+      go++;
+    }
+    if (answer5 === "cSharp") {
+      cSharp++;
+    }
+    if (answer5 === "javaScript") {
+      javaScript++;
+    }
+    if (answer5 === "go") {
+      go++;
     }
 
-  
+    if (cSharp > javaScript && cSharp > go) {
+      $("#result").show();
+      $("#result-text").text("cSharp");
+    }
+    if(javaScript > cSharp && javaScript > go) {
+      $("#result").show();
+      $("#result-text").text("JavaScript");
+    }
+    if (go > javaScript && go > cSharp) {
+      $("#result").show();
+      $("#result-text").text("Go");
+    }
+
     event.preventDefault();
   });
 });
